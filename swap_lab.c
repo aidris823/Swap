839319c11a2be6796bd23a00ba8f94ecbc8791e8
aidris823 <anandidris@Anands-MacBook-Pro.local>
2018-09-20 03:47:17 -0400

    Huh, that wasn't as bad as I thought it was.

--- swap_lab.c
+++ swap_lab.c
@@ -4,15 +4,23 @@
 
 int main(){
   srand(time(NULL));
-  // int RAND_PAUL = rand();
   int ary[10];
   for (int i = 0; i < 10; i++){
-    ary[i] = rand();
+    int RAND_PAUL = rand();
+    ary[i] = RAND_PAUL;    
   }
   ary[9] = 0;
   for (int i = 0; i < 10; i++){
     printf("The %d value: %d",i,ary[i]);
     printf("\n");
   }
+
+  //The Hard Part:
+  int revary[10];
+  for (int i = 0; i<  10; i++){
+    *(revary+i) = *(ary + 9 - i);
+    printf("The reverse of value of #%d : %d \n",i,*(revary+i));
+  }
+  
 }
   
